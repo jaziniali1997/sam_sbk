03e4d3a967b24dbae0b0d65ad1add5bc0fd0c1e2
Ali Jazini <43562013+jaziniali1997@users.noreply.github.com>
2023-12-12 15:42:27 +0330

    add daily_output_schemas

--- routers/v1/v1_schemas.py
+++ routers/v1/v1_schemas.py
@@ -7,4 +7,28 @@ class hourly_schemas(BaseModel):
 
 
 class daily_schemas(BaseModel):
+    date: dateclass daily_input_schemas(BaseModel):
     date: date
+
+
+class daily_data_schemas(BaseModel):
+    loc_id: int
+    date: datetime
+    t2m_dm: float
+    t2m_dmax: float
+    t2m_dmin: float
+    d2m_dm: float
+    tp_dsum: float
+    ssrd_dsum: float
+    e_dsum: float
+    sd_dsum: float
+    sde_dsum: float
+    rh2m_dmax: int
+    rh2m_dm: int
+    rh2m_dmin: int
+    sshn_dsum: float
+
+
+class daily_output_schemas(BaseModel):
+    date: date
+    result: List[daily_data_schemas]
